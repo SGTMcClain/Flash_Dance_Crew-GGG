0b672e01c9125c8878e1bf3d6c4ed76192ff43d8
cahnz1 <32645938+cahnz1@users.noreply.github.com>
2021-01-29 15:20:44 -0500

    Zippy's branch (#5)
    * main menu created but not finished
    
    * finished ui functionality.  needs art and content for controls
    
    * basic changes
    
    * lockable levels added.  needs win mechanic added

---
+++ NeonEmbrace/Assets/Scripts/UI/OpeningScreen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningScreen : MonoBehaviour
+{
+    public GameObject controlPanel;
+
+    public void play(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void quitGame()
+    {
+        //end this game's life
+        Debug.Log("gamequit...");
+        Application.Quit();
+    }
+
+    public void controls()
+    {
+        controlPanel.SetActive(!controlPanel.activeSelf);
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            controlPanel.SetActive(!controlPanel.activeSelf);
+
+        }
+    }
+
+    /* // Start is called before the first frame update
+     void Start()
+     {
+
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     }*/
+}
