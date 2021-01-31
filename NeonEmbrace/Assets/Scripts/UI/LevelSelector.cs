6e54608cc591c7f1e963420a5617ed3895c44852
cahnz1 <32645938+cahnz1@users.noreply.github.com>
2021-01-31 10:51:37 -0500

    added pause button to all levels except 5 and 1.  also added controles description and levels to level select

--- NeonEmbrace/Assets/Scripts/UI/LevelSelector.cs
+++ NeonEmbrace/Assets/Scripts/UI/LevelSelector.cs
@@ -2,13 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
+    //create array of levels
+    public Button[] levelButtons;
+
 
     public void Select(string levelName)
     {
         SceneManager.LoadScene(levelName);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //keeps track of what level we are on (starts at level 1)
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+
+        //disable all buttons but level 1 to start
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 1 > levelReached)
+            {
+                levelButtons[i].interactable = false;
+            }
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            Debug.Log("8 pressed");
+            //inable all buttons 
+            for (int i = 0; i < levelButtons.Length; i++)
+            {
+                levelButtons[i].interactable = true;
+            }
+        }
     }
 
 
